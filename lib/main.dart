0302c1da21d72ede9d3f4d3fd7e71cecba56239d
Akansha Sen <akanshasen@hotmail.com>
2021-09-27 12:30:25 +1300

    added the asset line in pubspec.lock and the MS logo

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,28 @@
+import 'package:flutter/material.dart';
 
-main(){
-
+void main() {
+  runApp(MsApp());
 }
 
-class 
+class MsApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Mobile Shielding'),
+        ),
+        body: Card(
+          child: Column(
+            children: <Widget>[
+              Image.asset('assets/MobileShieldingLogo.jpg'),
+              Text('Mobile Shielding Text')
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+//Scaffold creates a white background, allows for ability to add app bar etc.
+
+}
