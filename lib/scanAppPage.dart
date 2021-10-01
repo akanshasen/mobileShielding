d85d5111f75c285fb0cf44088b415ed401bd977d
Akansha Sen <akanshasen@hotmail.com>
2021-10-01 18:55:07 +1300

    tried to add timer for the progress bar, does not work yet

--- lib/scanAppPage.dart
+++ lib/scanAppPage.dart
@@ -1,53 +1,76 @@
 import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:mobileshielding/scanNoIssueFoundpage.dart';
 
 class ScanAppPage extends StatelessWidget {
+
+
   @override
   Widget build(BuildContext context) {
+    // Future.delayed(Duration(seconds: 0), (){
+    //   ElevatedButton(
+    //     onPressed: () {
+    //       Navigator.push(
+    //           context,
+    //           MaterialPageRoute(
+    //               builder: (context) => ScanAppPage()));
+    //     },
+    //     style: ElevatedButton.styleFrom(
+    //         primary: Colors.deepOrangeAccent),
+    //     child: Text(
+    //       'Scan App',
+    //       style: TextStyle(
+    //           color: Colors.white, fontSize: 23),
+    //     ),
+    //   );
+    // });
+    Timer _timer;
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Builder(
-          builder: (context) => Scaffold(
-            appBar: AppBar(
-              leading: IconButton(
-                icon: Icon(Icons.menu),
-                onPressed: () {},
-              ),
-              backgroundColor: Color(0xff790507),
-              title: Text('Scan Apps'),
+      debugShowCheckedModeBanner: false,
+      home: Builder(
+        builder: (context) => Scaffold(
+          appBar: AppBar(
+            leading: IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () {},
             ),
-            body: Container(
-                padding: EdgeInsets.all(16.0),
-                child: Column(
-                  children: <Widget>[
-                    Text("Scanning Apps", style: TextStyle(fontSize: 30)),
-                    SizedBox(
-                      height: 40.0,
-                      child: Center(
-                        child: _linearProgressIndicator(),
-                      ),
-                    ),
-                  ],
-                )),
+            backgroundColor: Color(0xff790507),
+            title: Text('Scan Apps'),
           ),
-        ));
+          body: Container(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              children: <Widget>[
+                Text("Scanning Apps", style: TextStyle(fontSize: 30)),
+                SizedBox(
+                  height: 40.0,
+                  child: Center(
+                    child: _linearProgressIndicator(),
+
+                  ),
+
+                ),
+
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+
   }
 
+
   Widget _linearProgressIndicator() {
+
     return LinearProgressIndicator(
+
       minHeight: 40.0,
-      color: Colors.red,
+      color: Colors.redAccent,
       semanticsLabel: "Scanning Apps",
-      backgroundColor: Colors.green,
+      backgroundColor: Colors.blueAccent,
     );
   }
 
-  // void initState() {
-  //   Timer(Duration(seconds: 3), () {
-  //     Navigator.pushReplacement(context,
-  //         MaterialPageRoute(builder: (context) => ScanNoIssueFound()));
-  //   });
-  //   super.initState();
-  // }
 }
